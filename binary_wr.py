2e167a50da7d7c7f601aafe7bfbe6df47c730d0a
AhmedSaid3617 <said.ahmed3617@gmail.com>
2025-08-19 16:17:30 +0300

    Added seed-key secure access validation.

--- binary_wr.py
+++ binary_wr.py
@@ -1,38 +1,10 @@
 import sys
 import subprocess
 from pathlib import Path
+from crc import Crc32
 import os
 
 HEADER_SIZE = 512
-
-# CRC32 class
-class Crc32:
-    crc_table = {}
-
-    def __init__(self, _poly):
-        # Generate CRC table for polynomial
-        for i in range(256):
-            c = i << 24
-            for j in range(8):
-                c = (c << 1) ^ _poly if (c & 0x80000000) else c << 1
-            self.crc_table[i] = c & 0xFFFFFFFF
-
-    # Calculate CRC from input buffer
-    def calculate(self, buf):
-        crc = 0xFFFFFFFF
-
-        i = 0
-        while i < len(buf):
-            b = [buf[i+3], buf[i+2], buf[i+1], buf[i+0]]
-            i += 4
-            for byte in b:
-                crc = ((crc << 8) & 0xFFFFFFFF) ^ self.crc_table[(crc >> 24) ^ byte]
-        return crc
-    
-    # Create bytes array from integer input
-    def crc_int_to_bytes(self, i):
-        return [(i >> 24) & 0xFF, (i >> 16) & 0xFF, (i >> 8) & 0xFF, i & 0xFF]
-    
 
 def elf_to_bin(elf_path: Path) -> Path:
     """Convert ELF to BIN using objcopy and return bin path."""
--- flash.py
+++ flash.py
@@ -1,13 +1,22 @@
 import serial
 import os
+from crc import Crc32
 
 CHUNK_SIZE = 1024  # 1KB
 TIMEOUT = 5  # 5 seconds
 
-# Protocol constants
-CMD_REQ_TO_SEND  = b"\xA1"
-CMD_OK_TO_SEND   = b"\xB1"
-CMD_SEND_NEXT = b"\xB2"
+CMD_REQ_TO_SEND = b'\xA1'
+CMD_SECURITY_ACCESS = b'\xA2'
+
+CMD_OK_TO_SEND = b'\xB1'
+CMD_SEND_NEXT = b'\xB2'
+CMD_LOADING_ERR = b'\xB3'
+CMD_APP_INVALID = b'\xB4'
+CMD_UNAUTHORIZED = b'\xB5'
+CMD_TIMEOUT = b'\xB6'
+CMD_PERMISSION_GRANTED = b'\xB7'
+
+SECRET_KEY = bytearray([0xDE, 0xAD, 0xBE, 0xEF])
 
 class Flasher:
     def __init__(self, port):
@@ -16,8 +25,28 @@ class Flasher:
     def flash(self, path):
         filesize = os.path.getsize(path)
         print(f"Flashing {path} of size {filesize} bytes")
+
+        # Send security access command.
+        self.ser.write(CMD_SECURITY_ACCESS)
+
+        # Get seed for CRC calculation
+        seed = self.ser.read(4)
+
+        # Calculate and send key.
+        crc_input = SECRET_KEY[::-1] + bytearray(seed)
+        crc = Crc32(0x04C11DB7)
+        crc_val = crc.calculate(crc_input)
+        print(hex(crc_val))
+        self.ser.write(crc_val.to_bytes(4, byteorder='little'))
+
+        # Wait for permission granted
+        ack = self.ser.read(1)
+        if ack != CMD_PERMISSION_GRANTED:
+            raise Exception("Failed to receive PERMISSION_GRANTED from device")
+
+        # Send request to send.
         self.ser.write(CMD_REQ_TO_SEND)
-        self.ser.write(filesize.to_bytes(4, byteorder='big'))
+        self.ser.write(filesize.to_bytes(4, byteorder='little'))
 
         ack = self.ser.read(1)
         if ack != CMD_OK_TO_SEND:
